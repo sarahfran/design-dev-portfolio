9a57588ebab3be81e205f22b3678d35d90f41603
sarahfran <sarahfrances.thompson@gmail.com>
2026-09-18 15:55:29 +1200

    Display dynamic projects data on ProjectDetail page, move assets from src to public/images

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom' // <-- Check spelling carefully
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,7 +1,31 @@
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import projects from "../data/projects.json"
+
 export default function ProjectDetail() {
+
+  const { id } = useParams();
+  const project = projects.find((project) => project.id === id);
+
   return (
+    <>
     <div>
-      <h1>Project Detail Page</h1>
+      <Link to="/">Back to projects</Link>
     </div>
+    <div>
+      <h1>{project.title}</h1>
+      <h2>{project.tagline}</h2>
+      <div className="">
+        {project.tags.map((tag) => (
+          <span key={tag} className="">
+            {tag}
+          </span>
+        ))}
+      </div>
+      <div>
+        <img src={project.heroImage} alt={project.title} />
+      </div> 
+    </div>
+    </>
   );
 }
