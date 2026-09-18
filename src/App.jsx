27d3d0910ce7003fb3bd9e63279ca417b67c8e80
sarahfran <sarahfrances.thompson@gmail.com>
2026-09-18 15:58:48 +1200

    Remove boilerplace CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import './App.css'
 function App() {
   return (
     <div className="app-container">
-      {/* 
-        The <Outlet /> component acts as a placeholder.
-        React Router will swap this out for your Home.jsx content 
-        or any other page you route to later.
-      */}
       <Outlet />
     </div>
   )
